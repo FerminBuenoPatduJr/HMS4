3d589739cf07bb2241234b01d565ab9555e9fb9a
FerminBuenoPatduJr <rajubueno411@gmail.com>
2022-12-27 20:00:40 +0800

    commit to main

---
+++ UserApp/src/main/java/com/gsi77/UserApp/UserAppApplication.java
@@ -0,0 +1,13 @@
+package com.gsi77.UserApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAppApplication.class, args);
+	}
+
+}
---
+++ UserApp/src/test/java/com/gsi77/UserApp/UserAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsi77.UserApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
